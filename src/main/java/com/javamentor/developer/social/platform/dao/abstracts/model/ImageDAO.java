83a6ecf6b2ad3712a052788b2596cd89f8ecd807
Damir Yusupov <usupovdamir1993@gmail.com>
2020-07-29 21:53:23 +0300

    Initial commit

---
+++ src/main/java/com/javamentor/developer/social/platform/dao/abstracts/model/ImageDAO.java
@@ -0,0 +1,12 @@
+package com.javamentor.developer.social.platform.dao.abstracts.model;
+
+
+import com.javamentor.developer.social.platform.models.entity.media.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface ImageDAO extends JpaRepository<Image, Long> {
+
+
+}
